5ddd52787511a9f870fbb8030b2254a03fa43d0d
Asus <Asus@Gokul>
2024-12-15 13:56:46 +0530

    Assignment commit

---
+++ C2TC_GOKULL/src/com/tns/ifet/assignment/AssignmentMain.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.assignment;
+import com.tns.ifet.assignment.employee.Manager;
+import com.tns.ifet.assignment.employee.Developer;
+import com.tns.ifet.assignment.utilities.EmployeeUtilities;
+public class AssignmentMain {
+ public static void main(String[] args) {
+ Manager manager = new Manager("John", 101, 90000, "Sales");
+ Developer developer = new Developer("Alice", 102, 80000, "Java");
+ EmployeeUtilities.printEmployeeDetails(manager);
+ EmployeeUtilities.printEmployeeDetails(developer);
+ }
+}
